ef7d6b5b2ab450addc4035b29f7e7f84918ae6f2
gsalah18 <40832531+gsalah18@users.noreply.github.com>
2018-09-06 18:00:13 +0300

    sss

--- Company/src/servlets/Tasks.java
+++ Company/src/servlets/Tasks.java
@@ -18,19 +18,16 @@ public class Tasks extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String userId = req.getParameter("user_id");
-		String userType = req.getParameter("user_type");
 		
 		if(req.getSession().getAttribute("userId") == null
 				|| req.getSession().getAttribute("userType") == null
-				|| (userId == null || userId.length() == 0)
-				|| (userType == null || userType.length() == 0)) {
+				|| (userId == null || userId.length() == 0)) {
 			resp.sendRedirect("index");
 			return;
 		}
 		
 		
 		List<Task>tasks=DatabaseUtil.getInstance().getUserTasks(userId);
-		req.setAttribute("userType", userType);
 		req.setAttribute("userId", userId);
 		req.setAttribute("tasks", tasks);
 		req.getRequestDispatcher("views/tasks.jsp").forward(req, resp);
